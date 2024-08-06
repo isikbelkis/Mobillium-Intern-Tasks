e11ff3ec2671c3361df3b44b88f2c652ff2e3e77
Belkıs Işık <isikbelkis1@gmail.com>
2024-08-07 00:34:53 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MoviesAppTask4A"
+include(":app")
+ 
